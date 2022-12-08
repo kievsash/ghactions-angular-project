f1433953c8783dbfa2c71785c1c508e4d926306c
Oleksandr Poshtaruk <kievsash@ukr.net>
2022-12-08 11:39:47 +0200

    memlab step - upload

--- memlab/check-leaks.js
+++ memlab/check-leaks.js
@@ -1,13 +1,19 @@
 // run scenario's with memlab
 // memlab run --scenario ./memlab-scenarios/scenario-1.js --work-dir ./results
 // node ./memlab-scenarios/check-leaks.js
-
+const fs = require('fs');
 const {scenario} = require('./scenario-1.js');
 const {findLeaks, BrowserInteractionResultReader, takeSnapshots, run} = require('@memlab/api');
 
 (async function () {
 
   const {leaks} = await run({scenario});
-  console.log('Number of leaks found', leaks.length);
+  // console.log('Number of leaks found', leaks.length);
+
+  fs.writeFile('./leaks-'+Date.now(), leaks.length, err => {
+    if (err) {
+      console.error(err);
+    }
+  });
 })();
 
